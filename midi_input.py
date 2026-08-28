67c6c8c185ebf91e29bd12027630af5cd9e7505b
Preston <42254676+GoldShots@users.noreply.github.com>
2026-08-28 17:40:46 -0500

    Add MIDI Listening

--- midi_input.py
+++ midi_input.py
@@ -1,6 +1,15 @@
 import mido
+import pynput
 
 print("MIDI devices: ")
 
 for device in mido.get_input_names():
     print(device)
+
+device = mido.get_input_names()[1]
+
+with mido.open_input(device) as port:
+    print(f"Listening to {device}...")
+
+    for message in port:
+        print(message)
