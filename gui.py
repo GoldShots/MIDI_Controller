de074844ec7cbbd1057a7061408b97d2d26cf60e
Preston <42254676+GoldShots@users.noreply.github.com>
2026-08-28 20:20:29 -0500

    Program Terminates When Closing Window

--- gui.py
+++ gui.py
@@ -2,11 +2,17 @@ from PySide6.QtWidgets import QApplication, QWidget, QLabel
 
 import sys
 
+class MainWindow(QWidget):
+
+    def closeEvent(self, event):
+        QApplication.quit()
+        event.accept()
+
 def create_gui(device, mapper):
 
     app = QApplication(sys.argv)
 
-    window = QWidget()
+    window = MainWindow()
 
     window.setWindowTitle("MIDI to Controller")
     window.resize(600, 400)
