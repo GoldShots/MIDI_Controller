cea02d3b40f992e4150c3f9b821a3d77eba2ab49
Preston <42254676+GoldShots@users.noreply.github.com>
2026-08-28 21:51:51 -0500

    Begin Mapping Editing

--- gui.py
+++ gui.py
@@ -1,5 +1,17 @@
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QTableWidgetItem, QTableWidget
+from PySide6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QTableWidget,
+    QTableWidgetItem,
+    QPushButton,
+    QDialog,
+    QVBoxLayout,
+    QComboBox
+)
 
+from PySide6.QtCore import Qt
+from pynput.mouse import Button
 import sys
 
 class MainWindow(QWidget):
@@ -17,6 +29,41 @@ def midi_note_name(note):
     octave = (int)(note / 12) - 1
 
     return f"{names[note % 12]}{octave}"
+
+def edit_mapping(table, mapper):
+    row = table.currentRow()
+
+    if row < 0:
+        return
+
+    note_item = table.item(row, 0)
+
+    if note_item is None:
+        return
+
+    note = note_item.data(Qt.UserRole)
+
+    dialog = QDialog(table)
+    dialog.setWindowTitle("Edit Mapping")
+
+    layout = QVBoxLayout(dialog)
+
+    output_type = QComboBox()
+    output_type.addItems([
+        "Keyboard",
+        "Mouse Button",
+        "Mouse Movement"
+    ])
+
+    layout.addWidget(output_type)
+
+    save_button = QPushButton("Save")
+    layout.addWidget(save_button)
+
+    save_button.clicked.connect(dialog.accept)
+
+    if dialog.exec():
+        ...
 
 def create_gui(device, mapper):
 
@@ -125,6 +172,16 @@ def create_gui(device, mapper):
             2,
             QTableWidgetItem(mapping)
         )
+
+    table.setSelectionBehavior(QTableWidget.SelectRows)
+    table.setEditTriggers(QTableWidget.NoEditTriggers)
+
+    edit_button = QPushButton("Edit Mapping", parent = window)
+    edit_button.move(20, 365)
+
+    edit_button.clicked.connect(
+        lambda: edit_mapping(table, mapper)
+    )
 
     window.show()
 
